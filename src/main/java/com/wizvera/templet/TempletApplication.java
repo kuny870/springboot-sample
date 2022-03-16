d81de805cf025596fd43e01e59d29ce89182c546
kuny870 <kuny87@naver.com>
2022-03-16 19:11:39 +0900

    20220316 keonhee synology 에 db 연결

--- src/main/java/com/wizvera/templet/TempletApplication.java
+++ src/main/java/com/wizvera/templet/TempletApplication.java
@@ -2,12 +2,9 @@ package com.wizvera.templet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 
 @SpringBootApplication
 public class TempletApplication {
-
-    CommonOAuth2Provider commonOAuth2Provider;
 
     public static void main(String[] args) {
         SpringApplication.run(TempletApplication.class, args);
